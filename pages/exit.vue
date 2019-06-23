c895bc2ded00a7a9b2a04cff89137ed02e125257
gmw-zjw <mingwei2222@126.com>
2019-06-23 15:32:43 +0800

    +exit

--- pages/exit.vue
+++ pages/exit.vue
@@ -1,9 +1,16 @@
 <template>
-  
+  <div></div>
 </template>
 
 <script>
 export default {
-  
+  layout: 'blank',
+  // 使用中间件实现退出
+  middleware:  async () => {
+    let { status, data } = await this.$axios.get('/users/exit')
+    if (status === 200 && data && data.code === 0) {
+      window.location.href = '/'
+    }
+  }
 }
 </script>
