21ad6317d6a74a7aa97847c55a8e8f9f1c82b72a
gmw-zjw <mingwei2222@126.com>
2019-06-23 15:29:08 +0800

    +signin

--- components/public/header/user.vue
+++ components/public/header/user.vue
@@ -18,6 +18,12 @@ export default {
     return {
       user: ''
     }
+  },
+  methods: async () => {
+    const {status, data: {user}} = await this.$axios.get('/users/getUser')
+    if(status === 200) {
+      location.href = '/'
+    }
   }
 }
 </script>
